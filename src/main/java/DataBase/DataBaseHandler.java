31bb60928004ba4a5736c2592394e261f17b7a56
Bernie B. Tuwain <bernie.tuwain@got-the-experts.com>
2019-03-01 10:57:08 +0800

    latest commit

--- src/main/java/DataBase/DataBaseHandler.java
+++ src/main/java/DataBase/DataBaseHandler.java
@@ -790,7 +790,6 @@ public class DataBaseHandler extends SQLiteOpenHelper {
                 myQuery = myQuery + " Order BY AccountID, cast(IFNULL(SequenceNo, 99999) as REAL) ASC ";
             }
         }
-
         //Log.e(TAG,"here: "+ myQuery);
 
         Cursor c = sql.rawQuery(myQuery, null);
